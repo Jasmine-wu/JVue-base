421227a87cd42eaeadbfd842111452f7edf1f3a4
jasmine <18674601207@163.com>
2021-08-10 07:52:56 +0800

    add vue.config.js to modify serve port

--- vue-demo/src/main.js
+++ vue-demo/src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue' //引入Vue对象,相当于<script src="vue.js"></script>
+import App from './App.vue' //引入Aapp.vue 里的App对象，项目的入口页面
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.config.productionTip = false //控制台有一条提示信息
+
+new Vue({ //实例Vue
+        render: h => h(App), //渲染App页面
+    }).$mount('#app') //渲染到index.html里面id=app标签内
---
+++ vue-demo/vue.config.js
@@ -0,0 +1,9 @@
+// vue默认的配置文件
+
+// webpack+node环境导出配置对象
+module.exports = {
+    devServer: {
+        port: 4444,
+        open: true, //是否浏览器自动打开
+    }
+}
